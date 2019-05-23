790c5798df5c512f8a3ca43da84b8ddc09d6e463
tengdepeng <tengdepeng@zolbon.com>
2019-05-23 18:09:31 +0800

    ++

--- src/components/zuji.js
+++ src/components/zuji.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React,{Component} from "react"
 
  
@@ -12,18 +11,3 @@ class welcome extends Component{
 }
 
 export default welcome
-=======
-import React,{Component} from "react"
-
- 
-class welcome extends Component{
-	render(){
-		return(<div>
-			<h2 style={{color:'red'}}>足迹 </h2>
-			<p>我去过北上广  闯过加勒比  </p>
-			</div>)
-	}
-}
-
-export default welcome
->>>>>>> 1165554db5bbdc92f828cd5e027947fae02eea76
